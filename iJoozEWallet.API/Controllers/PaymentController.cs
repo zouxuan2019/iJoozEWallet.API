8ebd59d6d5de2f2844a57cc00405b7524f54a64f
zouxuan <zouxuan@dbs.com>
2019-10-13 23:38:13 +0800

    Add PaymentController for fomo payment callback

---
+++ iJoozEWallet.API/Controllers/PaymentController.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iJoozEWallet.API.Domain.Services;
+using iJoozEWallet.API.Resources;
+using iJoozEWallet.API.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace iJoozEWallet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IEWalletService _eWalletService;
+        private readonly ILogger _logger;
+
+        public PaymentController(ILoggerFactory depLoggerFactory, IEWalletService eWalletService)
+        {
+            _logger = depLoggerFactory.CreateLogger("Controllers.PaymentController");
+            _eWalletService = eWalletService;
+        }
+
+        [HttpPost("callback")]
+        public async void ProcessPaymentCallback([FromForm] FomoPayResponse fomoPayResponse)
+        {
+            _logger.LogInformation("processPaymentCallback Request:" + JsonConvert.SerializeObject(fomoPayResponse));
+            if (!IsSignatureValid(fomoPayResponse)) return;
+
+            var resource = await ConvertFomoPayResponseToTopUpResource(fomoPayResponse);
+            if (resource != null)
+            {
+                await _eWalletService.SaveTopUpAsync(resource);
+            }
+        }
+
+        private async Task<TopUpResource> ConvertFomoPayResponseToTopUpResource(FomoPayResponse fomoPayResponse)
+        {
+            var topUpHistory = await _eWalletService.FindByTopUpTransactionIdAsync(fomoPayResponse.transaction);
+            var initTransaction = topUpHistory.First(x => x.Status == Status.Init);
+            if (initTransaction != null)
+            {
+                return new TopUpResource
+                {
+                    TransactionId = fomoPayResponse.transaction,
+                    ActionDate = DateTime.Now,
+                    Status = fomoPayResponse.result == "0" ? Status.Success : Status.Fail,
+                    PaymentReferenceNo = fomoPayResponse.payment_id + '|' + fomoPayResponse.upstream + '|' +
+                                         fomoPayResponse.nonce + '|' +
+                                         fomoPayResponse.signature,
+                    UserId = initTransaction.UserId,
+                    Amount = initTransaction.Amount,
+                    PaymentMerchant = initTransaction.PaymentMerchant
+                };
+            }
+
+            return null;
+        }
+
+        private static string ComputeSha256Hash(string rawData)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            return hash.ToString().ToLower();
+        }
+
+        private bool IsSignatureValid(FomoPayResponse fomoPayResponse)
+        {
+            var param = new SortedDictionary<string, string>
+            {
+                {"transaction", fomoPayResponse.transaction},
+                {"result", fomoPayResponse.result},
+                {"upstream", fomoPayResponse.upstream},
+                {"nonce", fomoPayResponse.nonce},
+                {"payment_id", fomoPayResponse.payment_id}
+            };
+            var strArray = new StringBuilder();
+            foreach (var (key, value) in param)
+            {
+                strArray.Append(key + "=" + value + "&");
+            }
+
+            var nLen = strArray.Length;
+            strArray.Remove(nLen - 1, 1);
+            var computeSha256Hash = ComputeSha256Hash(strArray.ToString());
+            _logger.LogInformation(
+                $"computedSignature:{computeSha256Hash},originalSignature:{fomoPayResponse.signature}");
+            return computeSha256Hash == fomoPayResponse.signature;
+        }
+    }
+
+
+    public class FomoPayResponse
+    {
+        public string transaction { get; set; }
+        public string result { get; set; }
+        public string upstream { get; set; }
+        public string nonce { get; set; }
+        public string payment_id { get; set; }
+        public string signature { get; set; }
+    }
+}
